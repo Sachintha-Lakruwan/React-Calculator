eeec1c8f0db94084ef45ffa49487736997b8e68a
Sachintha-Lakruwan <sachi.new2002@gmail.com>
2024-06-13 02:52:51 +0530

    display overflow hided

--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -2,7 +2,7 @@ import { useAppSelector } from "../store/hooks";
 export default function Display() {
   const text = useAppSelector((state) => state.calculator.expression);
   return (
-    <div className=" bg-background-screen rounded-md flex justify-end items-center px-9">
+    <div className=" bg-background-screen rounded-md flex justify-end items-center px-9 overflow-hidden">
       <h2 className=" text-display-text text-5xl font-semibold">{text}</h2>
     </div>
   );
